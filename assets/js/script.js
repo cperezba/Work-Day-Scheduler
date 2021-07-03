42ed9c9ddc7aa413bf4438f3dbe2941b480d66a7
Carlos Perez <ca.perez.barr@gmail.com>
2021-07-03 01:17:10 -0500

    Began local storage, troubleshooting script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,3 +27,31 @@ function timeColoring() {
 }
 
 timeColoring();
+
+
+$(saveBtn).on("click", function() {
+    var varValue = $(this).siblings(".textValue").val()
+    var varKey = $(this).parent().attr("id")
+
+    window.localStorage.setItem(varKey, varValue)
+})
+
+$("#0 .textValue").val(window.localStorage.getItem("0"))
+$("#1 .textValue").val(window.localStorage.getItem("1"))
+$("#2 .textValue").val(window.localStorage.getItem("2"))
+$("#3 .textValue").val(window.localStorage.getItem("3"))
+$("#4 .textValue").val(window.localStorage.getItem("4"))
+$("#5 .textValue").val(window.localStorage.getItem("5"))
+$("#6 .textValue").val(window.localStorage.getItem("6"))
+$("#7 .textValue").val(window.localStorage.getItem("7"))
+$("#8 .textValue").val(window.localStorage.getItem("8"))
+
+
+$("#1 .varValue").val(window.localStorage.getItem("0"))
+$("#2 .varValue").val(window.localStorage.getItem("0"))
+$("#3 .varValue").val(window.localStorage.getItem("0"))
+$("#4 .varValue").val(window.localStorage.getItem("0"))
+$("#5 .varValue").val(window.localStorage.getItem("0"))
+$("#6 .varValue").val(window.localStorage.getItem("0"))
+$("#7 .varValue").val(window.localStorage.getItem("0"))
+$("#8 .varValue").val(window.localStorage.getItem("0"))

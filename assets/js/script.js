07a1a5607b4b69ef1ce62a76b479240c75e498f3
Carlos Perez <ca.perez.barr@gmail.com>
2021-07-02 23:47:31 -0500

    Added colored time stamps

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,29 @@
+var today = moment();
+var currentHour = today.hours();
+
 function update() {
-$('#currentDay').html(moment().format('D. MMMM YYYY H:mm:ss'));
+    $('#currentDay').html(moment().format('D. MMMM YYYY hh:mm:ss'));
 }
-  
+
 setInterval(update, 1000);
+
+
 
 var saveBtn = $('#saveBtn');
 
 
+function timeColoring() {
+    $('.time-block').each(function(){
+        var timeSection = $(this).attr("id")
+        if (timeSection < currentHour) {
+            $(this).addClass("past")
+        } else if (timeSection == currentHour) {
+            $(this).addClass("present")
+        } else {
+            $(this).removeClass("past present")
+            $(this).addClass("future")
+        }
+    })
+}
+
+timeColoring();
